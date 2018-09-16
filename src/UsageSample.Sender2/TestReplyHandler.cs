b5a693c7dcc0961397d026bb09dc5a194e3ffe1c
Jeremy Stafford <dasjestyr@gmail.com>
2018-09-16 15:07:29 -0700

    cleanup

--- src/UsageSample.Sender2/TestReplyHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using NServiceBus.Callbacks.Redis;
-using UsageSample.Messaging;
-
-namespace UsageSample.Sender2
-{
-    // We only declare these at all so that NSB will register them as handlers during assembly scan 
-
-    public class TestReplyHandler : MessageCallback<TestReply> { }
-}
